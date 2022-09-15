25b18d322a75ce1a1724a6707afe6a24fd128f0a
Aymeric <aymeric.goudout@ens.uvsq.fr>
2022-09-15 09:46:05 +0200

    update

---
+++ TDs/TD1/exo2.py
@@ -0,0 +1,3 @@
+naissance = int(input("Donnez votre date de naissance svp :  "))
+l_animaux = ["singe", "coq", "chien", "porc", "rat", "boeuf", "tigre", "lapin", "dragon", "serpent", "cheval", "mouton"]
+print(l_animaux[naissance%12])
