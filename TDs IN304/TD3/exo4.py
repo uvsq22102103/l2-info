972e2dfd0fb8b9cb9a869cd22b8dd280ac4ea149
Aymeric <aymeric.goudout@ens.uvsq.fr>
2022-09-23 18:54:34 +0200

    Update exo4.py

--- TDs IN304/TD3/exo4.py
+++ TDs IN304/TD3/exo4.py
@@ -2,22 +2,27 @@ import string, random
 
 
 def groupage(eleves:list,n):
-    g = int(len(eleves)/n)+1 # doit être conditionnel
-    groupes = []
-    while len(eleves) >= g:
-        groupes.append([])
-        l = len(groupes)-1
-        for i in range(g):
-            e = random.choice(eleves)
-            eleves.remove(e)
-            groupes[l].append(e)
-    groupes.append(eleves)
+    groupes = [[] for i in range(n)]
+    while len(eleves) > 0:
+        for i in range(n):
+            if len(eleves) != 0:
+                e = random.choice(eleves)
+                eleves.remove(e)
+                groupes[i].append(e)
     return(groupes)
-    
-
 
 
 nb_etudiants = 15
 etudiants = list(string.ascii_lowercase[:nb_etudiants])
 print(etudiants)
-print(groupage(etudiants,5))
+groupes = groupage(etudiants.copy(),5)
+
+dico1 = {}
+for i in range(len(groupes)):
+    dico1[i+1] = groupes[i]
+print(dico1)
+dico2 = {}
+for i in range(len(groupes)):
+    dico2[i+1] = {"membres":groupes[i],"notes":[random.randint(0,20) for i in range(len(groupes[i]))]}
+    dico2[i+1]["moyenne"] = round(sum(dico2[i+1]["notes"])/len(dico2[i+1]["notes"]),2)
+print(dico2)
