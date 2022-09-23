2b8b30cc0e1a956f7d41d10e5e55bc362de74189
Aymeric <aymeric.goudout@ens.uvsq.fr>
2022-09-23 17:30:13 +0200

    Create horloge_dodo.py

---
+++ Outils/horloge_dodo.py
@@ -0,0 +1,20 @@
+import os
+import time
+
+def countdown(t):
+	
+	while t:
+		mins, secs = divmod(t, 60)
+		timer = '{:02d}:{:02d}'.format(mins, secs)
+		print(timer, end="\r")
+		time.sleep(1)
+		t -= 1
+	
+	os.system("shutdown /s /t 1")
+
+
+# input time in seconds
+t = input("Enter the time in seconds: ")
+
+# function call
+countdown(int(t))
