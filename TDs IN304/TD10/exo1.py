03cd4985f13be3de8cd0a32b3ea226989d690142
Aymeric <aymeric.goudout@ens.uvsq.fr>
2022-09-28 22:04:19 +0200

    Create exo1.py

---
+++ TDs IN304/TD10/exo1.py
@@ -0,0 +1,28 @@
+class Titre:
+    def __init__(self,titre:str):
+        self.__titre = titre
+    
+    def getTitre(self):
+        return(self.__titre)
+    
+    def Afficher(self):
+        print(self.__titre)
+
+
+class Annee:
+    def __init__(self,annee:int):
+        self.__annee = annee
+
+    def getAnnee(self):
+        return(self.__annee)
+    
+    def Afficher(self):
+        print(self.__annee)
+
+
+class CD(Titre,Annee):
+    def __init__(self,titre:str,annee:int,nomArtiste:str,nbreTitres:int):
+        self.__nomArtiste = nomArtiste
+        self.__nbreTitres = nbreTitres
+        Titre.__init__(self,titre)
+        Annee.__init__(self,annee)
