8b6b0fa75e4bbcfcbedea36c9e8814f4a3c8498e
Aymeric <aymeric.goudout@ens.uvsq.fr>
2022-09-29 23:24:04 +0200

    Update exo1.py

--- TDs IN304/TD10/exo1.py
+++ TDs IN304/TD10/exo1.py
@@ -28,8 +28,20 @@ class CD(Titre,Annee):
         Annee.__init__(self,annee)
     
     def Afficher(self):
-        print(self.__nomArtiste+str(self.__nbreTitres))
-        super().Afficher()
+        print(self.__nomArtiste+" : "+Titre.getTitre(self)+" ; %d titres ; sorti en %d"%(self.__nbreTitres,Annee.getAnnee(self)))
+
+
+class DVD(Titre,Annee):
+    def __init__(self,titre:str,annee:int,realisateur:str):
+        self.__real = realisateur
+        Titre.__init__(titre)
+        Annee.__init__(annee)
+    
+    def Afficher(self):
+        print("Nom : "+Titre.getTitre(self)+"; réalisateur "+self.__real+" ; Parution en %d"%Annee.getAnnee(self))
 
 
 cd1 = CD("Feu de bois",2020,"Damso",11)
+dvd1 = DVD("Intouchables",2012,"Patrick MURET")
+dvd1.Afficher()
+cd1.Afficher()
