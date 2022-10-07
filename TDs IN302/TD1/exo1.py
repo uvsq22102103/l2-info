8ed776a5baedf6d95edd37995755742fc312b9c0
Aymeric <aymeric.goudout@ens.uvsq.fr>
2022-10-07 16:12:43 +0200

    Update exo1.py

--- TDs IN302/TD1/exo1.py
+++ TDs IN302/TD1/exo1.py
@@ -1,4 +1,5 @@
-class Nombre:
+
+class Nombre_entier:
     def __init__(self,valeur:int,base:int=10):
         if valeur < 0:
             self.signe = 1
@@ -29,8 +30,8 @@ class Nombre:
             return output
     
     def turn_to_baseb(self, baseb:int):
-        nba = Nombre(self.value,self.base).decimal()
-        return Nombre(nba).decimal_to_base(baseb)
+        nba = Nombre_entier(self.value,self.base).decimal()
+        return Nombre_entier(nba).decimal_to_base(baseb)
     
     def complement_a_un(nb:str):
         output = ""
@@ -56,14 +57,14 @@ class Nombre:
         return output[::-1]
     
     def complement_a_x(nb:str,x:int = 2):
-        nb = Nombre.complement_a_un(nb)
+        nb = Nombre_entier.complement_a_un(nb)
         for i in range(x-1):
-            nb = Nombre.__complement(nb)
+            nb = Nombre_entier.__complement(nb)
         return nb
 
     
     def r_entier_signés(self, nb_bits:int = 8):
-        bits = Nombre(self.value,self.base).turn_to_baseb(2)
+        bits = Nombre_entier(self.value,self.base).turn_to_baseb(2)
         if len(bits) > nb_bits:
             print("l'allocation de bits n'est pas suffisante")
         else:
@@ -75,23 +76,49 @@ class Nombre:
     def BDC(self):
         output = ""
         for i in str(self.value):
-            nb = Nombre(int(i)).decimal_to_base(2)
+            nb = Nombre_entier(int(i)).decimal_to_base(2)
             while len(nb) < 4:
                 nb = "0" + nb
             output += nb
         return output
+    
+    def addition_bits(nb1:str,nb2:str):
+        total = Nombre_entier(int(nb1),2).decimal() + Nombre_entier(int(nb2),2).decimal()
+        return Nombre_entier(total).decimal_to_base(2)
+    
+    def multiplication_bits(nb1:str,nb2:str):
+        total = Nombre_entier(int(nb1),2).decimal() * Nombre_entier(int(nb2),2).decimal()
+        return Nombre_entier(total).decimal_to_base(2)
 
-# à faire :  addition et multiplication d'entiers binaire
-#            codage des flottants, conversion des flottants, addition et multiplication.
+
+class Nombre_flottant:
+    """Base 10 seulement"""
+    def __init__(self,nb:str):
+        if nb[0] == "-":
+            nb = nb[1:]
+            self.signe = 1
+        else:
+            self.signe = 0
+        self.value = nb
+        nb = nb.split(".")
+        self.arrondi = nb[0]
+        self.fractionnaire = nb[1]
+
+#à faire :    codage des flottants, conversion des flottants, addition et multiplication.
 
 
-nb = Nombre(-48)
+nb = Nombre_entier(-48)
 print(nb.decimal())
 print(nb.decimal_to_base(2))
 print(nb.turn_to_baseb(2))
 test = nb.r_entier_signés()
 print(test)
-print(Nombre.complement_a_un(test))
-print(Nombre.complement_a_x(test,2))
-print(Nombre.complement_a_x(test,3))
+print(Nombre_entier.complement_a_un(test))
+print(Nombre_entier.complement_a_x(test,2))
+print(Nombre_entier.complement_a_x(test,3))
 print(nb.BDC())
+print(Nombre_entier.addition_bits("1111","1000"))
+print(Nombre_entier.multiplication_bits("1111","1000"))
+test = Nombre_flottant("-416244.521656")
+print(test.arrondi)
+print(test.fractionnaire)
