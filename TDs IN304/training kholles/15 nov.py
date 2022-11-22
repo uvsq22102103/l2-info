d24774f2220158c43be13444205fbb010cd2bb31
Aymeric <aymeric.goudout@ens.uvsq.fr>
2022-11-22 09:48:35 +0100

    update

---
+++ TDs IN304/training kholles/15 nov.py
@@ -0,0 +1,13 @@
+# révisions sur les dictionnaires
+dico = {"hervé":46}
+dico.fromkeys()
+dico.copy()
+dico.keys()
+dico.values()
+dico.items()
+dico.pop()
+dico.popitem()
+dico.setdefault()
+dico.update()
+dico.clear()
+
