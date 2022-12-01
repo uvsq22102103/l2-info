979d04f77bf239ad6e7e358970501598441dedb1
Aymeric <aymeric.goudout@ens.uvsq.fr>
2022-12-01 10:36:12 +0100

    backend finalised for TD11-12

--- TDs IN304/TD11-12/InPoDa.py
+++
@@ -1,5 +0,0 @@
-# Plateforme fictive analysant les tweets pour faire de la donnée
-# La zone d'atterrissage contient des élément
-
-import pandas as pd
-df_tweets = pd.read_json("TDs IN304/TD11-12/versailles_tweets_100.json")
