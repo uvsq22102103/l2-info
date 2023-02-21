4aa6570cd575fcefce79a23ba3fe0d3c4caa4768
Aymeric <aymeric.goudout@ens.uvsq.fr>
2023-02-21 15:38:31 +0100

    tok

--- IN405/TD4/test.py
+++ IN405/TD4/test.py
@@ -20,18 +20,19 @@ def mode_octal_to_str(octal_value:str):
 
 def dir_all_list(path_in):
     """return mode user size last_access_time name"""
-    stat = os.stat(path_in)
-    mode = 0
-    for i, value in enumerate(oct(stat.st_mode)[-3:]):
-        mode += int(value)*(8**(2-i))
-    mode = mode_octal_to_str(mode)
-    user = getpwuid(stat.st_uid).pw_name
-    size = str(stat.st_size)
-    date = gmtime(stat.st_atime)
     mois = {1:"Jan.",2:"Fev.",3:"Mars",4:"Avril",5:"Mai",6:"Juin",
             7:"Juillet",8:"Août",9:"Sept.",10:"Oct.",11:"Nov.",12:"Dec."}
-    last_access_time = str(date.tm_mday)+" "+mois[date.tm_mon]+" "+str(date.tm_year)
-    name = os.path.basename(path_in)
-    return " ".join([mode,user,size,last_access_time,name])
+    for path in os.listdir(path_in):
+        stat = os.stat(path)
+        mode = 0
+        for i, value in enumerate(oct(stat.st_mode)[-3:]):
+            mode += int(value)*(8**(2-i))
+        mode = mode_octal_to_str(mode)
+        user = getpwuid(stat.st_uid).pw_name
+        size = str(stat.st_size)
+        date = gmtime(stat.st_atime)
+        last_access_time = str(date.tm_mday)+" "+mois[date.tm_mon]+" "+str(date.tm_year)
+        name = os.path.basename(path)
+        print (" ".join([mode,user,size,last_access_time,name]))
 
-print(dir_all_list("./"))
+dir_all_list("./")
